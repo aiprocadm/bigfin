76ffa40aab59fb8f63bf497013947ea453378d11
aiproc <aiproc.adm@gmail.com>
2026-06-23 05:41:53 +0300

    feat(zenmoney): страница подключения и импорта операций (фронт, ⑨b)

--- packages/webapp/src/constants/features.tsx
+++ packages/webapp/src/constants/features.tsx
@@ -12,4 +12,5 @@ export const Features = {
   InterfaceModes: 'interface_modes',
   FinancialModel: 'financial_model',
   BankStatementImport: 'bank_statement_import',
+  Zenmoney: 'zenmoney_import', // ⑨b импорт Дзенмани
 }
--- packages/webapp/src/constants/sidebarMenu.tsx
+++ packages/webapp/src/constants/sidebarMenu.tsx
@@ -822,6 +822,12 @@ export const SidebarMenu = [
     feature: Features.FinancialModel,
   },
   {
+    text: <T id={'sidebar.zenmoney'} />,
+    href: '/zenmoney',
+    type: ISidebarMenuItemType.Link,
+    feature: Features.Zenmoney,
+  },
+  {
     text: <T id={'sidebar.system'} />,
     type: ISidebarMenuItemType.Group,
     children: [
